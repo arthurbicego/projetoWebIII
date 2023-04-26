5bbb62a60053b202e830a41cd5444822be47704f
arthurbicego <106209566+arthurbicego@users.noreply.github.com>
2023-04-26 18:56:14 -0300

    Delete projeto directory

--- projeto/src/main/java/tech/ada/mercado/repository/MercadoRepository.java
+++
@@ -1,7 +0,0 @@
-package tech.ada.mercado.repository;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import tech.ada.mercado.model.Mercado;
-
-public interface MercadoRepository extends ReactiveMongoRepository<Mercado,String> {
-}
